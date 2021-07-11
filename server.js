b26e7fbbb486b34c2215e13d62d32bf0052fb998
rashikh <rashikh@outlook.com>
2021-07-11 03:18:20 +0200

    Code refactored.

--- server.js
+++ server.js
@@ -1,81 +1,37 @@
-const express = require('express')
-const SparqlClient = require('sparql-http-client')
-const cors = require('cors')
-const app = express()
-let fetchedData = []
+const express = require('express');
+const SparqlClient = require('sparql-http-client');
+const cors = require('cors');
+const app = express();
+let fetchedData = [];
+
 app.listen(8000, () => {
-    console.log('Server started!')
+    console.log('Server is running!');
 })
 
-/* async function asyncCall() {
-    console.log('Data call initiated:');
-    const client = new SparqlClient({ endpointUrl: 'http://localhost:3030/dataset' })
-    const stream = await client.query.select('PREFIX owl: <http://www.w3.org/2002/07/owl#>\
-    select distinct  ?subject ?predicate ?object {\
-     ?subject a owl:Class; \
-        ?predicate ?object \
-      FILTER(!isBlank(?subject))}')
-    console.log('Data loaded:');
-
-    var fetchedData = []
-    stream.on('data', quad => {
-        //console.log(`${quad.subject.value} + ' # ' + ${quad.predicate.value} + ' # ' + ${quad.object.value}`)
-        fetchedData.push({ subject: quad.subject.value, predicate: quad.subject.predicate, object: quad.subject.object })
-    })
-    return fetchedData
-}
-
-app.route('/api/cats').get((req, res) => {
-    data = asyncCall();
-    console.log(data)
-    res.send({
-        data: data,
-    })
-})
-
-app.get("/getdata", async function (req, res) {
-    // var data = await pullData();
-    // var filteredData = await filterByYear(data);
-    // res.json(filteredData);
-    data = await asyncCall();
-    console.log(data)
-    res.send({
-        data: data,
-    })
-}) */
 let asyncCall = new Promise(async function (Resolve, Reject) {
     console.log('Data call initiated:');
-    const client = new SparqlClient({ endpointUrl: 'http://localhost:3030/dataset' })
+    const client = new SparqlClient({ endpointUrl: 'http://localhost:3030/dataset' });
     const stream = await client.query.select('PREFIX owl: <http://www.w3.org/2002/07/owl#>\
     select distinct  ?subject ?predicate ?object {\
      ?subject a owl:Class; \
         ?predicate ?object \
-      FILTER(!isBlank(?subject))}')
-    console.log('Data loaded:');
+      FILTER(!isBlank(?subject))}');
 
-    stream.on('data', quad => {
-        //console.log(`${quad.subject.value} + ' # ' + ${quad.predicate.value} + ' # ' + ${quad.object.value}`)
-        fetchedData.push({ subject: quad.subject.value, predicate: quad.predicate.value, object: quad.object.value })
+    //console.log('Data stream loaded!');
+
+    stream.on('data', row => {
+        //console.log(`${row.subject.value} + ' # ' + ${row.predicate.value} + ' # ' + ${row.object.value}`);
+        fetchedData.push({ subject: row.subject.value, predicate: row.predicate.value, object: row.object.value });
     })
     stream.on('end', function () {
-        Resolve(); // when successful
+        Resolve();
     })
-    //return fetchedData
 
-    //myReject();  // when error
+    //Reject(); 
 });
 
-
-app.route('/api/cats').get((req, res) => {
-    data = asyncCall();
-    console.log(data)
-    res.send({
-        data: data,
-    })
-})
-
 app.get("/getdata", async function (req, res) {
- res.header('Access-Control-Allow-Origin', '*');	
+    res.header('Access-Control-Allow-Origin', '*');
     asyncCall.then(function () {
         res.send({
             data: fetchedData,
@@ -83,5 +39,5 @@ app.get("/getdata", async function (req, res) {
     });
 })
 
-app.use(cors())
+app.use(cors());
 
