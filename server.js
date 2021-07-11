0fbd22d9ad1e3117721990f1289f5e6748a38842
rashikh <rashikh@outlook.com>
2021-07-11 15:38:27 +0200

    Error handling enabled on data fetch.

--- server.js
+++ server.js
@@ -8,28 +8,6 @@ app.listen(8000, () => {
     console.log('Server is running...');
 })
 
-let asyncCallToLoadData = new Promise(async function (Resolve, Reject) {
-    //console.log('Data call initiated!');
-    const client = new SparqlClient({ endpointUrl: 'http://localhost:3030/dataset/' });
-    const stream = await client.query.select('PREFIX owl: <http://www.w3.org/2002/07/owl#>\
-    select distinct  ?subject ?predicate ?object {\
-     ?subject a owl:Class; \
-        ?predicate ?object \
-      FILTER(!isBlank(?subject))}');
-
-    //console.log('Data stream loaded!');
-
-    stream.on('data', row => {
-        //console.log(`${row.subject.value} + ' # ' + ${row.predicate.value} + ' # ' + ${row.object.value}`);
-        fetchedData.push({ subject: row.subject.value, predicate: row.predicate.value, object: row.object.value });
-    })
-    stream.on('end', function () {
-        Resolve();
-    })
-
-    //Reject(); 
-});
-
 app.get("/", async function (req, res) {
     res.header('Access-Control-Allow-Origin', '*');
     res.send({
@@ -43,8 +21,35 @@ app.get("/getdata", async function (req, res) {
         res.send({
             data: fetchedData,
         })
+    }).catch(function (error) {
+        res.status(500).json({ Error: 'Internal server error' });
     });
 })
+
+let asyncCallToLoadData = new Promise(async function (Resolve, Reject) {
+    try {
+        //console.log('Data call initiated!');
+        const client = new SparqlClient({ endpointUrl: 'http://localhost:3030/dataset/' });
+        const stream = await client.query.select('PREFIX owl: <http://www.w3.org/2002/07/owl#>\
+                        select distinct  ?subject ?predicate ?object {\
+                        ?subject a owl:Class; \
+                        ?predicate ?object \
+                        FILTER(!isBlank(?subject))}');
+
+        //console.log('Data stream loaded!');
+
+        stream.on('data', row => {
+            //console.log(`${row.subject.value} + ' # ' + ${row.predicate.value} + ' # ' + ${row.object.value}`);
+            fetchedData.push({ subject: row.subject.value, predicate: row.predicate.value, object: row.object.value });
+        })
+        stream.on('end', function () {
+            Resolve();
+        })
+    } catch (error) {
+        //console.error("Error Message: " + error.message);
+        Reject("Error Message: " + error.message);
+    }
+});
 
 app.use(cors());
 
